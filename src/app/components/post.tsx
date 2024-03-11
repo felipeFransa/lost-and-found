e155ad10fd6e06c886444eaf037c753c62a640f0
Felipe <antoniofelipesilva@live.com>
2024-03-11 16:58:33 -0300

    post structure

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,5 +1,15 @@
-const post = () => {
+export const Post = () => {
     return (
-        <div></div>
+        <div className="flex-col justify-center items-center">
+            <div className="w-96 h-96 flex-col justify-center items-center bg-blue-700 rounded-md">
+                <div className="w-96 h-20 flex justify-center items-center">
+                    <div className="w-80 h-20 flex justify-between items-center">
+                        <div className="w-16 h-16 rounded-full bg-white"></div>
+                        <div className="w-9/12 h-16 rounded-md bg-green-500"></div>
+                    </div>
+                </div>
+                <div className=""></div>
+            </div>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@ import { Header } from "@/app/components/Header";
 import { UserCard } from "@/app/components/userCard"
 import { NewPost } from "@/app/components/newPost"
 import { Adverts } from "@/app/components/adverts"
+import { Post } from "@/app/components/post"
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="">
       <Header />
       <div className="flex justify-center bg-white">
         <UserCard />
         <NewPost />
         <Adverts />
+      </div>
+      <div className="flex justify-center bg-white">
+        <Post />
       </div>
     </div>
   );
