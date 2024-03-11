6002e488e7bf0f597bf5a997c88aed9f552dbc96
Felipe <antoniofelipesilva@live.com>
2024-03-11 16:13:15 -0300

    user structure

--- src/app/components/userCard.tsx
+++ src/app/components/userCard.tsx
@@ -1,5 +1,7 @@
-const userCard = () => {
+export const UserCard = () => {
     return (
-        <div></div>
+        <div className="w-96 h-96 flex justify-center items-center bg-gradient-to-r from-orange-600 to-blue-500 rounded-md">
+            <div className="w-44 h-44 rounded-full bg-slate-50"></div>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import { Header } from "@/app/components/Header"
+import { Header } from "@/app/components/Header";
+import { UserCard } from "@/app/components/userCard"
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="flex">
+        <UserCard />
+      </div>
     </div>
   );
 }
