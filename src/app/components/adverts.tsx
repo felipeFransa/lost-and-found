b0dc6fa8d4b6b6d9e901d3b8bd76179c9ea0247b
Felipe <antoniofelipesilva@live.com>
2024-03-11 16:39:53 -0300

    adverts structure

--- src/app/components/adverts.tsx
+++ src/app/components/adverts.tsx
@@ -1,3 +1,9 @@
-const adverts = () => {
-    <div></div>
+export const Adverts = () => {
+    return (
+        <div className="w-96 h-96 bg-slate-500 rounded-md flex justify-center items-center">
+            <div>
+                Anucios....
+            </div>
+        </div>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Header } from "@/app/components/Header";
 import { UserCard } from "@/app/components/userCard"
 import { NewPost } from "@/app/components/newPost"
+import { Adverts } from "@/app/components/adverts"
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto">
       <Header />
-      <div className="flex justify-around">
+      <div className="flex justify-center bg-white">
         <UserCard />
         <NewPost />
+        <Adverts />
       </div>
     </div>
   );
