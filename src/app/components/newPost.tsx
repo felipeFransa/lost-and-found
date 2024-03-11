437e9a634435fe9e63dfe83e2b4227e43d90561c
Felipe <antoniofelipesilva@live.com>
2024-03-11 16:32:31 -0300

    newPost structure

--- src/app/components/newPost.tsx
+++ src/app/components/newPost.tsx
@@ -1,5 +1,21 @@
-const newPost = () => {
+export const NewPost = () => {
     return (
-        <div></div>
+        <div className="w-96 h-24 flex justify-between items-center bg-white rounded-md">
+            <div className="w-4/12 flex justify-center">
+                <div className="w-16 h-16 bg-blue-700 rounded-full"></div>
+            </div>
+            <div className="w-8/12 h-24 flex-col justify-center">
+                <div className="w-full h-12 flex justify-center items-center">
+                    <div className="w-60 h-10 bg-blue-700 rounded"></div>
+                </div>
+                <div className="w-full h-12 flex justify-around items-center rounded-md">
+                    <div className="w-8 h-8 bg-cyan-500 rounded-md"></div>
+                    <div className="w-8 h-8 bg-cyan-500 rounded-md"></div>
+                    <div className="w-8 h-8 bg-cyan-500 rounded-md"></div>
+                    <div className="w-20 h-8 bg-cyan-500 rounded-md"></div>
+                </div>
+
+            </div>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Header } from "@/app/components/Header";
 import { UserCard } from "@/app/components/userCard"
+import { NewPost } from "@/app/components/newPost"
+
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="flex">
+      <div className="flex justify-around">
         <UserCard />
+        <NewPost />
       </div>
     </div>
   );
